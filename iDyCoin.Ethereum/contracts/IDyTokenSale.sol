26e6c6ad2932346213604ede58659fde1e0507e2
PiotrApriasz <PPlacki500>
2022-02-24 13:06:50 +0100

    Add IDyTokenSale contract AB#9

---
+++ iDyCoin.Ethereum/contracts/IDyTokenSale.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.0;
+
+import "./Crowdsale.sol";
+
+contract IDyTokenSale is Crowdsale {
+
+    constructor(
+        uint256 rate,
+        address payable wallet,
+        IERC20 token
+    ) Crowdsale(rate, wallet, token) {
+
+    }
+}
