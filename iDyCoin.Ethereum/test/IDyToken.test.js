e75ec768dbf25331042302f1e2a82858407fa0cf
PiotrApriasz <PPlacki500>
2022-02-24 12:15:39 +0100

    Add more tests AB#6

--- iDyCoin.Ethereum/test/IDyToken.test.js
+++ iDyCoin.Ethereum/test/IDyToken.test.js
@@ -10,10 +10,30 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
+
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
     it("all tokens should be in my account", async () => {
         let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
         expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+    })
+
+    it("is possible to send tokens between accounts", async () => {
+        const tokensAmount = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        expect(instance.transfer(recipient, tokensAmount)).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(tokensAmount)));
+        expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(tokensAmount));
+    })
+
+    it("is not possible to send more tokens than available in total", async () => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+        expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.eventually.be.fulfilled;
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
     })
     
 });
