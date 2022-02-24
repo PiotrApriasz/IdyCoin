dabc254f3e71db66a8c6942cfda1f4c389365fda
PiotrApriasz <PPlacki500>
2022-02-24 13:44:00 +0100

    Update migration file AB#9

--- iDyCoin.Ethereum/contracts/IDyTokenSale.sol
+++ iDyCoin.Ethereum/contracts/IDyTokenSale.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 
 import "./Crowdsale.sol";
 
-contract IDyTokenSale is Crowdsale {
+contract IDyTokenSale is Crowdsale { 
 
     constructor(
         uint256 rate,
--- iDyCoin.Ethereum/migrations/2_deploy_contracts.js
+++ iDyCoin.Ethereum/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var IDyToken = artifacts.require("iDyToken.sol");
+var IDyTokenSale = artifacts.require("IDyTokenSale");
 
 module.exports = async function(deployer) {
+    let addr = await web3.eth.getAccounts();
     await deployer.deploy(IDyToken, 1000000 );
+    await deployer.deploy(IDyTokenSale, 1, addr[0], IDyToken.address);
+    let instance = await IDyToken.deployed();
+    await instance.transfer(IDyTokenSale.address, 1000000);
 }
