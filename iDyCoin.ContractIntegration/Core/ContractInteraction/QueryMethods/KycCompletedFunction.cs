8d5c43193e4fe8442945a6b007b085a4348105c0
PiotrApriasz <PPlacki500>
2022-02-27 15:59:43 +0100

    Add core queries methods classes AB#13

---
+++ iDyCoin.ContractIntegration/Core/ContractInteraction/QueryMethods/KycCompletedFunction.cs
@@ -0,0 +1,11 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+
+namespace iDyCoin.ContractIntegration.Core.ContractInteraction.QueryMethods;
+
+[Function("kycCompleted", "bool")]
+public class KycCompletedFunction : FunctionMessage
+{
+    [Parameter("address", "_addr", 1)]
+    public string Addr { get; set; }
+}
