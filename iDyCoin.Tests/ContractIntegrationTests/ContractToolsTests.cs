1e3f77b785cbbe078b4448099c2e9839bbbeda90
PiotrApriasz <PPlacki500>
2022-03-03 13:19:47 +0100

    Add kyc test AB#19

--- iDyCoin.Tests/ContractIntegrationTests/ContractToolsTests.cs
+++ iDyCoin.Tests/ContractIntegrationTests/ContractToolsTests.cs
@@ -10,6 +10,6 @@ public class ContractToolsTests
     public void GetContractAddress_ValidPath_ReturnsDploymentAddress()
     {
         var address = ContractTools.GetContracAddress<IDyToken>("5777");
-        Assert.Equal("0x88Ee2A6A5692a486D8a3775365F03fb0Fa990712", address);
+        Assert.Equal("0x6423f396a2d13FdBEc44CC76beD3F4f55f145aC4", address);
     }
 }
--- iDyCoin.Tests/ContractIntegrationTests/KycContractTest.cs
+++ iDyCoin.Tests/ContractIntegrationTests/KycContractTest.cs
@@ -1,13 +1,26 @@
-﻿using Xunit;
+﻿using iDyCoin.ContractIntegration.Contracts;
+using Xunit;
 
 namespace iDyCoin.Tests.ContractIntegrationTests;
 
 public class KycContractTest
 {
     [Fact]
-    public void SetKyc_ValidAddress_VerifyAddressCorrectly()
+    public async void SetKyc_ValidAddress_VerifyAddressCorrectly()
     {
-        
+        var kycContract = new KycContract();
+        await kycContract.SetKycCompleted("0x496C22EAAD3AC714a047EA5F981aF7Bb1D7dFf15");
+        var kycCompleted = await kycContract.KycCompleted("0x496C22EAAD3AC714a047EA5F981aF7Bb1D7dFf15");
+        Assert.True(kycCompleted);
+    }
+
+    [Fact]
+    public async void RevokeKyc_ValidAddress_RevokedAddressCorrectly()
+    {
+        var kycContract = new KycContract();
+        await kycContract.SetKycRevoked("0x496C22EAAD3AC714a047EA5F981aF7Bb1D7dFf15");
+        var kycCompleted = await kycContract.KycCompleted("0x496C22EAAD3AC714a047EA5F981aF7Bb1D7dFf15");
+        Assert.False(kycCompleted);
     }
     
 }
